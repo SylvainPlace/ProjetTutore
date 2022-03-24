a69203872a8d6d23cc5f36b74a6e47d55a2dcdde
Sylvain Place <sylvain.place@etud.univ-jfc.fr>
2022-03-24 16:26:46 +0100

    Ajout Soigner

---
+++ backend/src/main/java/fr/jfc/ptut/entity/Soigner.java
@@ -0,0 +1,48 @@
+package fr.jfc.ptut.entity;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import fr.jfc.ptut.enume.*;
+import java.lang.Enum;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+// Un exemple d'entité
+// On utilise Lombok pour auto-générer getter / setter / toString...
+// cf. https://examples.javacodegeeks.com/spring-boot-with-lombok/
+@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+@Entity // Une entité JPA
+public class Soigner {
+    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Integer id;
+/*
+    @NonNull
+    @ManyToOne(optional = false) 
+    private Medicament medicament;
+    @NonNull
+    @ManyToOne(optional = false) 
+    private Utilisateur utilisateur;
+    @NonNull
+    @ManyToOne(optional = false) 
+    private Maladie maladie;
+*/
+    private int valduree;
+    @Enumerated(EnumType.ORDINAL)
+    private UniteDuree uniteduree;
+    private int valfreq;
+    @Enumerated(EnumType.ORDINAL)
+    private UniteFreq unitefreq;
+    private int doseparprise;
+
+}
