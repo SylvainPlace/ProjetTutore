9713f0b5ace949f69c1425a9d1bb12eb18cfe42e
Raynal12 <raynal.mathilde12@gmail.com>
2022-03-31 15:39:43 +0200

    debut footer mais pas affichage

---
+++ frontend/src/components/footer.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <p>Vous aimez ?</p>
+	<input type="image" id="image" src="Téléchargements/pouceVert.png" alt="Bouton pouce vert"/>
+	<input type="image" id="image" src="Téléchargements/pouceRouge.png" alt="Bouton pouce rouge"/>
+
+
+    <p>Five Cure</p>
+    <p>de la Team</p>
+    <p>Polypathologique</p>
+    <p>Nous contacter</p>
+    <p>adresseMail@àNousDeDecider</p>
+
+
+    <!-- Bouton -->
+	<input type="image" id="image" value="Revenir en haut de page" src="Téléchargements/flechePtut.png" alt="Fleche sur laquelle cliquer pour revenir au haut de page "/>
+    
+
+</template>
--- frontend/src/views/Accueil.vue
+++ frontend/src/views/Accueil.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <footer/>
 </template>
 
 <script setup>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import footer from "@/components/footer.vue"
 </script>
