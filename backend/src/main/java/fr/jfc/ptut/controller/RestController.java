7a63c9880043eb4e84d381eab4e09fc6f2cd99b6
FlorianChavernac <florian.chavernac@etud.univ-jfc.fr>
2022-04-05 11:11:04 +0200

    Ajout d'API

--- backend/src/main/java/fr/jfc/ptut/controller/RestController.java
+++ backend/src/main/java/fr/jfc/ptut/controller/RestController.java
@@ -1,5 +1,6 @@
 package fr.jfc.ptut.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +18,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import lombok.extern.slf4j.Slf4j;
 
 import fr.jfc.ptut.dao.MaladieRepository;
+import fr.jfc.ptut.dao.MedicamentRepository;
 import fr.jfc.ptut.entity.Maladie;
-
-
+import fr.jfc.ptut.entity.Medicament;
 import fr.jfc.ptut.dao.SoignerRepository;
 import fr.jfc.ptut.dto.CityForm;
 import fr.jfc.ptut.dto.PopulationResult;
-import fr.jfc.ptut.entity.Soigner;
 
+import fr.jfc.ptut.entity.Soigner;
+import fr.jfc.ptut.enume.Categorie;
+import fr.jfc.ptut.enume.Cim;
+import fr.jfc.ptut.enume.UniteDuree;
+import fr.jfc.ptut.enume.UniteFreq;
 
 @Controller // This means that this class is a Controller
 @RequestMapping(path = "/api") // This means URL's start with /rest (after Application path)
@@ -34,68 +39,123 @@ public class RestController {
 	@Autowired
 	private MaladieRepository maladieDao;
 
+	@Autowired
+	private MedicamentRepository medicamentDao;
 
 	@Autowired
 	private SoignerRepository soignerDao;
-	
-
 
 	/**
 	 * Enregistre une ville dans la base
-	 * Requête HTTP POST à l'URL http://localhost:8989/rest/saveCity	
+	 * Requête HTTP POST à l'URL http://localhost:8989/rest/saveCity
+	 * 
 	 * @param laVille la ville à enregistrer, initialisée par Spring à partir des
 	 *                valeurs transmises dans la requête POST
 	 *                Spring fera une requête SQL INSERT ou UPDATE pour enregistrer
 	 *                la ville dans la base
 	 * @return la ville enregistrée (avec sa clé) en format JSON
-     */
+	 */
 	/*
-	@PostMapping(path = "saveCity") 
-	public @ResponseBody City enregistreUneVille(@RequestBody CityForm formData) {
-		log.info("Reçu: {}", formData);
-		Country c = countryDao.findById(formData.getCountry())
-		   .orElseThrow(() -> new IllegalArgumentException("Pays inconnu"));
-		City laVille = new City(formData.getName(), c);
-		laVille.setPopulation(formData.getPopulation());
-		cityDao.save(laVille);
-		log.info("Enregistré: {}", laVille);
-		return laVille;
-	}
-
-	/**
+	 * @PostMapping(path = "saveCity")
+	 * public @ResponseBody City enregistreUneVille(@RequestBody CityForm formData)
+	 * {
+	 * log.info("Reçu: {}", formData);
+	 * Country c = countryDao.findById(formData.getCountry())
+	 * .orElseThrow(() -> new IllegalArgumentException("Pays inconnu"));
+	 * City laVille = new City(formData.getName(), c);
+	 * laVille.setPopulation(formData.getPopulation());
+	 * cityDao.save(laVille);
+	 * log.info("Enregistré: {}", laVille);
+	 * return laVille;
+	 * }
+	 * 
+	 * /**
 	 * Enregistre un pays dans la base
-	 * Requête HTTP POST à l'URL http://localhost:8989/rest/saveCountry	
-	 * @param lePays  le pays à enregistrer, initialisée par Spring à partir des
-	 *                valeurs  transmises dans la requête POST
-	 *                Spring fera une requête SQL INSERT ou UPDATE pour enregistrer
-	 *                le pays dans la base
+	 * Requête HTTP POST à l'URL http://localhost:8989/rest/saveCountry
+	 * 
+	 * @param lePays le pays à enregistrer, initialisée par Spring à partir des
+	 * valeurs transmises dans la requête POST
+	 * Spring fera une requête SQL INSERT ou UPDATE pour enregistrer
+	 * le pays dans la base
+	 * 
 	 * @return le pays enregistré (avec sa clé) en format JSON
-	 
-	@PostMapping(path = "saveCountry") 
-	public @ResponseBody Country enregistreUnPays(@RequestBody Country lePays) {
-		log.info("Reçu: {}", lePays);
-		countryDao.save(lePays);
-		log.info("Enregistré: {}", lePays);
-		return lePays;
+	 * 
+	 * @PostMapping(path = "saveCountry")
+	 * public @ResponseBody Country enregistreUnPays(@RequestBody Country lePays) {
+	 * log.info("Reçu: {}", lePays);
+	 * countryDao.save(lePays);
+	 * log.info("Enregistré: {}", lePays);
+	 * return lePays;
+	 * }
+	 */
+
+	@GetMapping(path = "medicamentsByName")
+	public @ResponseBody List<Medicament> medicamentsByName(String mot) {
+		List<Medicament> allMedicament = this.allMedicaments();
+		List<Medicament> searchMedicament = new ArrayList<>();
+		for (Medicament m : allMedicament) {
+			log.info(m.getNom_medic());
+			if (m.getNom_medic().contains(mot)) {
+				searchMedicament.add(m);
+			}
+		}
+		log.info(searchMedicament.toString());
+
+		return searchMedicament;
 	}
 
-	@GetMapping(path = "population") 
-	public @ResponseBody List<PopulationResult> populationParPays() {
-		log.info("Population pour chaque les pays");
-		return countryDao.populationParPaysJPQL();
-	}
-
-	@GetMapping(path = "allCities") 
-	public @ResponseBody List<City> allCities() {
+	@GetMapping(path = "allMedicaments")
+	public @ResponseBody List<Medicament> allMedicaments() {
 		log.info("Renvoie la liste des villes");
-		return cityDao.findAll();
+		return medicamentDao.findAll();
 	}
 
-	@GetMapping(path = "allCountries") 
-	public @ResponseBody List<Country> allCountries() {
-		log.info("Renvoie la liste des pays");
-		return countryDao.findAll();
+	@GetMapping(path = "allCategorie")
+	public @ResponseBody List<Categorie> allCategorie() {
+		log.info("Renvoie la liste des villes");
+		List<Categorie> lesCategories = new ArrayList<>();
+		for (Categorie c : Categorie.values()) {
+			lesCategories.add(c);
+		}
+		return lesCategories;
 	}
 
-*/
+	@GetMapping(path = "allCim")
+	public @ResponseBody List<Cim> allCim() {
+		log.info("Renvoie la liste des villes");
+		List<Cim> lesCims = new ArrayList<>();
+		for (Cim c : Cim.values()) {
+			lesCims.add(c);
+		}
+		return lesCims;
+	}
+
+	@GetMapping(path = "allUniteDuree")
+	public @ResponseBody List<UniteDuree> allUniteDuree() {
+		log.info("Renvoie la liste des villes");
+		List<UniteDuree> lesUniteDuree = new ArrayList<>();
+		for (UniteDuree c : UniteDuree.values()) {
+			lesUniteDuree.add(c);
+		}
+		return lesUniteDuree;
+	}
+
+	@GetMapping(path = "allUniteFreq")
+	public @ResponseBody List<UniteFreq> allUniteFreq() {
+		log.info("Renvoie la liste des villes");
+		List<UniteFreq> lesUniteFreq = new ArrayList<>();
+		for (UniteFreq c : UniteFreq.values()) {
+			lesUniteFreq.add(c);
+		}
+		return lesUniteFreq;
+	}
+
+	/*
+	 * @GetMapping(path = "allCountries")
+	 * public @ResponseBody List<Country> allCountries() {
+	 * log.info("Renvoie la liste des pays");
+	 * return countryDao.findAll();
+	 * }
+	 */
+
 }
--- backend/src/main/java/fr/jfc/ptut/dao/MedicamentRepository.java
+++ backend/src/main/java/fr/jfc/ptut/dao/MedicamentRepository.java
@@ -1,8 +1,10 @@
 package fr.jfc.ptut.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import fr.jfc.ptut.entity.Medicament;
 
 public interface MedicamentRepository extends JpaRepository<Medicament, Integer> {
-
 }
