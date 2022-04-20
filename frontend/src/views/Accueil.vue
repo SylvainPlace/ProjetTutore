e8a1fe5322ca814f8b93c02caa17bc2dd220f8d9
Raynal12 <raynal.mathilde12@gmail.com>
2022-04-20 14:19:31 +0200

    confits dans accueil

--- frontend/src/views/Accueil.vue
+++ frontend/src/views/Accueil.vue
@@ -1,14 +1,13 @@
 <template>
   
-  
+  <div class="home espace">
     <Accueil6 />
-  <h1>Bienvenue sur l'application WEB de FiveCure !</h1>
-  <p>Ce site WEB vous permettra de gérer les ordonnaces mieux que quiconque. <br> Une prise en main facilitée expliquée en quelques images </p>
-  <video controls poster="video.jpg" width="300">
+    <h1>Bienvenue sur l'application WEB de FiveCure !</h1>
+    <p>Ce site WEB vous permettra de gérer les ordonnaces mieux que quiconque. <br> Une prise en main facilitée expliquée en quelques images </p>
+    <video controls poster="video.jpg" width="300">
     <source src="@/assets/video.mp4"/>
-  </video>
-    <Footer/>
-
+    </video>
+  </div>
 </template>
 
 <script setup>
@@ -20,9 +19,3 @@ import Footer from "@/components/Footer.vue"
 <style scoped>
  
 </style>
-
-<template>
-  <div class="home espace">
-    
-  </div>
-</template>
