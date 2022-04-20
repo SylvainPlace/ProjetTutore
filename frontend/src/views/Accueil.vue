fa341d4bece6ad943fc8c3e0123ab2fb933bfa1d
CHABUS Océane <chabus.oceane@hotmail.com>
2022-04-20 14:52:39 +0200

    Style accueil

--- frontend/src/views/Accueil.vue
+++ frontend/src/views/Accueil.vue
@@ -1,11 +1,9 @@
 <template>
   
-  
-    <Accueil6 />
   <h1>Bienvenue sur l'application WEB de FiveCure !</h1>
 
   <p>Ce site WEB vous permettra de gérer les ordonnaces mieux que quiconque. <br> Une prise en main facilitée expliquée en quelques images </p>
-  <video controls poster="video.jpg" width="300">
+  <video controls poster="video.jpg">
     <source src="@/assets/video.mp4"/>
   </video>
     <Footer/>
@@ -14,7 +12,7 @@
 
 <script setup>
 // @ is an alias to /src
-import Accueil6 from "@/components/Accueil6.vue";
+
 import Footer from "@/components/Footer.vue"
 
 </script>
@@ -25,19 +23,23 @@ import Footer from "@/components/Footer.vue"
    background-color: #B48B75;
    width: 832px;
    position :absolute;
-   top : 206px;
-   left: 258px;
+   top: 242px;
+   left: 265px;
  }
 
  p{
  font-family: Avenir, Helvetica, Arial, sans-serif;
   font-size: 18px;
   color:black;
+  position :absolute;
+  top: 302px;
+  left: 265px;
+ }
+
+ video{
+ width : 450px;
+ position :absolute;
+ top: 396px;
+ left: 466px;
  }
 </style>
-
-<template>
-  <div class="home espace">
-    
-  </div>
-</template>
