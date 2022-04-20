843d80f743f0d40783c7fb775af217dcabbd2696
CHABUS Océane <chabus.oceane@hotmail.com>
2022-04-20 17:58:52 +0200

    Style infirmier

--- frontend/src/views/Infirmier.vue
+++ frontend/src/views/Infirmier.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="container espace">
-    <AddPatient />
-    <DetailPatient />
-
-    <DetailMaladie />
+    <div id="both">
+      <AddPatient />
+      <div id="detail">
+      <DetailPatient />
+      </div>
+    </div>
+    <div id="maladie">
+    <DetailMaladie /></div>
   </div>
 
 </template>
@@ -33,8 +37,9 @@ select#maladieSelect{
   background: -webkit-linear-gradient(to top, #f5bba0, #D09478);
   background: linear-gradient(to top, #D09478, #f5bba0);
 }
-table{
+div.py-5.table{
   border : black 3px solid ;
+  width : 400px; 
 }
 
 thead{
@@ -43,8 +48,21 @@ thead{
   background: linear-gradient(to top, #D09478, #f5bba0);
   
 }
-
 tbody{
   color : #5F4850;
+}
+#both{
+  display : flex;
+}
+
+#detail{
+  width: 1000px;
+  position: relative;
+  top: -38px;
+}
+
+#maladie{
+  position : relative;
+  top : 5px;
 }
 </style>
